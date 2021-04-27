d2fe70bfca775281567c2d704fb841496570a525
Matt Morse <mmorse1217@gmail.com>
2021-04-27 14:29:45 -0400

    adding symlink for python3 executable.

--- programs/python.sh
+++ programs/python.sh
@@ -5,6 +5,7 @@ echo "Installing python..."
   sudo bash Miniconda3-latest-Linux-x86_64.sh -ub 
   echo 'export PATH="~/miniconda3/bin:$PATH"' | cat - ~/.bashrc > /tmp/out 
   mv /tmp/out ~/.bashrc 
+  ln -s ~/miniconda3/bin/python3 /usr/bin/python
   rm Miniconda3-latest-Linux-x86_64.sh
   export PATH=~/miniconda3/bin:$PATH
 #fi
