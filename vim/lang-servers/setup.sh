c63a61f04038f224c2f376f0a45597eecb6d0757
Matt Morse <mmorse1217@users.noreply.github.com>
2021-12-09 16:08:34 -0500

    Update setup.sh
    updated version of nodejs for coc.nvim

--- vim/lang-servers/setup.sh
+++ vim/lang-servers/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt update
 sudo apt install -y nodejs yarn
