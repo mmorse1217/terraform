a6dbf7608c59d6e6164ec6ddf541961553a23b89
Matthew Morse <mmorse1217@gmail.com>
2023-05-30 23:09:36 -0400

    updated spacemacs install script.

--- programs/spacemacs.sh
+++ programs/spacemacs.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 echo "Installing Spacemacs..."
+echo "Installing dependencies..."
+apt update 
+apt install -y git ca-certificates --no-install-recommends
+
+echo "Backing up .emacs.d and .emacs..."
 cd ~
+if [ -d .emacs.d ]; then
 mv .emacs.d .emacs.d.bak
+fi
+
+if [ -d .emacs ]; then
 mv .emacs .emacs.bak
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
+
+echo "Installing...."
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d 
+
+rm -rf /var/lib/apt/lists/*
