7903463fa082bcd17831283f8a9bb761dff0576f
Matthew Morse <mmorse1217@gmail.com>
2023-05-30 22:44:33 -0400

    updated Anki install for ubuntu:latest

--- programs/anki.sh
+++ programs/anki.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 echo "Installing Anki..."
-wget https://github.com/ankitects/anki/releases/download/2.1.22/anki-2.1.22-linux-amd64.tar.bz2
-tar xjf anki-2.1.22-linux-amd64.tar.bz2
-cd anki-2.1.22-linux-amd64
+apt update 
+apt install -y wget zstd make sudo --no-install-recommends
+rm -rf /var/lib/apt/lists/*
+
+echo "Downloading Anki..."
+wget https://github.com/ankitects/anki/releases/download/2.1.64/anki-2.1.64-linux-qt6.tar.zst --no-check-certificate
+
+echo "Extracting Anki..."
+tar --use-compress-program=unzstd -xvf anki-2.1.64-linux-qt6.tar.zst
+cd anki-2.1.64-linux-qt6
+
+echo "Installing Anki..."
 sudo make install
+bash install
+
+echo "Anki Clean up..."
 cd ..
-rm -rf anki-2.1.22-linux-amd64
-rm -f anki-2.1.22-linux-amd64.tar.bz2
+rm -rf anki-2.1.64-linux-qt6
+rm -f anki-2.1.64-linux-qt6.tar.zst
 
