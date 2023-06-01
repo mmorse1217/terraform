692669b1fcbfd655c80ae1ceaa5e2d48ee241b62
Matthew Morse <mmorse1217@gmail.com>
2023-05-31 21:55:33 -0400

    adding autojump config

--- programs/autojump.sh
+++ programs/autojump.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Install autojump
+bash programs/python.sh
 echo "Installing autojump..."
+apt update
+apt install -y git ca-certificates --no-install-recommends
+rm -rf /var/lib/apt/lists/*
+
+
+git clone https://github.com/wting/autojump.git
 cd autojump
-./install.py
+SHELL=bash ./install.py
+
+echo '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' | cat - ~/.bashrc > /tmp/out 
+mv /tmp/out ~/.bashrc 
 cd ../../
 
